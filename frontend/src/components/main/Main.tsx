85e8bb3d4540b920a337407dcd5b8875c2af1bfe
shmen-shmen <akshevchenko@proton.me>
2024-07-19 19:26:04 +0500

    add grid & card system for main

--- frontend/src/components/main/Main.tsx
+++ frontend/src/components/main/Main.tsx
@@ -1,11 +1,91 @@
 import { Fragment } from "react";
 import NavbarComponent from "./NavbarComponent";
 import { Outlet } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+const Grid = () => {
+	return (
+		<Container fluid>
+			<Row>
+				<Col>
+					<Card style={{ width: "18rem" }}>
+						<Card.Img variant="top" src="holder.js/100px180" />
+						<Card.Body>
+							<Card.Title>Card Title</Card.Title>
+							<Card.Text>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col>
+					<Card style={{ width: "18rem" }}>
+						<Card.Img variant="top" src="holder.js/100px180" />
+						<Card.Body>
+							<Card.Title>Card Title</Card.Title>
+							<Card.Text>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col>
+					<Card style={{ width: "18rem" }}>
+						<Card.Img variant="top" src="holder.js/100px180" />
+						<Card.Body>
+							<Card.Title>Card Title</Card.Title>
+							<Card.Text>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col>
+					<Card style={{ width: "18rem" }}>
+						<Card.Img variant="top" src="holder.js/100px180" />
+						<Card.Body>
+							<Card.Title>Card Title</Card.Title>
+							<Card.Text>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col>
+					<Card style={{ width: "18rem" }}>
+						<Card.Img variant="top" src="holder.js/100px180" />
+						<Card.Body>
+							<Card.Title>Card Title</Card.Title>
+							<Card.Text>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
 
 export const News = () => {
 	return (
 		<Fragment>
 			<h2>News</h2>
+			<Grid />
 		</Fragment>
 	);
 };
@@ -13,6 +93,7 @@ export const TheyNeedHelp = () => {
 	return (
 		<Fragment>
 			<h2>TheyNeedHelp</h2>
+			<Grid />
 		</Fragment>
 	);
 };
@@ -20,6 +101,7 @@ export const WeHelpedThem = () => {
 	return (
 		<Fragment>
 			<h2>WeHelpedThem</h2>
+			<Grid />
 		</Fragment>
 	);
 };
@@ -27,6 +109,7 @@ export const AboutUs = () => {
 	return (
 		<Fragment>
 			<h2>AboutUs</h2>
+			<Grid />
 		</Fragment>
 	);
 };
@@ -35,7 +118,9 @@ export function Main() {
 	return (
 		<main>
 			<NavbarComponent></NavbarComponent>
-			<Outlet></Outlet>
+			<div className="container">
+				<Outlet></Outlet>
+			</div>
 		</main>
 	);
 }
